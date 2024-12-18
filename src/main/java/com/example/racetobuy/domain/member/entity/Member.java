cef90c28885aa6feba9697fe856ae366b390888f
Hwiyoung Jeon <wjsgnldud@naver.com>
2024-12-18 19:50:36 +0900

    feat: 회원 엔티티 벨리데이션 추가 & 도커.yml 추가
    - MEMBER 엔티티 벨리데이션 추가
    - docker_compse.yml값 커밋

--- src/main/java/com/example/racetobuy/domain/member/entity/Member.java
+++ src/main/java/com/example/racetobuy/domain/member/entity/Member.java
@@ -4,6 +4,7 @@ import com.example.racetobuy.domain.order.Order;
 import com.example.racetobuy.domain.timestamp.TimeStamp;
 import com.example.racetobuy.domain.wishlist.Wishlist;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,18 +23,23 @@ public class Member extends TimeStamp {
     @Column(name = "member_id")
     private Long memberId;
 
+    @NotBlank(message = "이름은 필수 입력 항목입니다.")
     @Column(name = "username", nullable = false, length = 50)
     private String username;
 
+    @NotBlank(message = "이메일은 필수 입력 항목입니다.")
     @Column(name = "email", nullable = false, unique = true, length = 100)
     private String email;
 
+    @NotBlank(message = "비밀번호는 필수 입력 항목입니다.")
     @Column(name = "password", nullable = false, length = 255)
     private String password;
 
+    @NotBlank(message = "전화번호는 필수 입력 항목입니다.")
     @Column(name = "phone_number", nullable = false, length = 20)
     private String phoneNumber;
 
+    @NotBlank(message = "주소는 필수 입력 항목입니다.")
     @Column(name = "address", nullable = false, columnDefinition = "TEXT")
     private String address;
 
