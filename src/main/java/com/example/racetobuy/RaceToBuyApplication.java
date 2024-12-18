84414cd800d77492031735eab203802e743e52de
Hwiyoung Jeon <wjsgnldud@naver.com>
2024-12-18 11:25:58 +0900

    Initial commit

---
+++ src/main/java/com/example/racetobuy/RaceToBuyApplication.java
@@ -0,0 +1,13 @@
+package com.example.racetobuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaceToBuyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaceToBuyApplication.class, args);
+    }
+
+}
